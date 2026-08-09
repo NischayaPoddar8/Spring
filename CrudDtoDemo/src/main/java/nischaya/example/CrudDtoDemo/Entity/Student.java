4fa840e5d33b47330595968a6b2ef026d45b71ac
Nischaya Poddar <nischayapoddar8@gmail.com>
2026-08-09 19:19:32 +0530

    Started learning about dto

--- CrudDtoDemo/src/main/java/nischaya/example/CrudDtoDemo/Entity/Student.java
+++ CrudDtoDemo/src/main/java/nischaya/example/CrudDtoDemo/Entity/Student.java
@@ -1,4 +1,18 @@
 package nischaya.example.CrudDtoDemo.Entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Student {
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+    private int rollNo;
+    private String emailId;
+    private String subject;
+    private boolean deleted;
 }
