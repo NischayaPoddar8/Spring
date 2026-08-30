163419faf81ba1ad844efba8d5568d674a0c8aea
Nischaya Poddar <nischayapoddar8@gmail.com>
2026-08-30 12:24:16 +0530

    Started practice project on dto filters and interceptors

---
+++ 4JobTrackerApi/src/main/java/nischaya/example/_JobTrackerApi/Application.java
@@ -0,0 +1,13 @@
+package nischaya.example._JobTrackerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ 4JobTrackerApi/src/main/java/nischaya/example/_JobTrackerApi/dto/hi.java
@@ -0,0 +1,4 @@
+package nischaya.example._JobTrackerApi.dto;
+
+public class hi {
+}
