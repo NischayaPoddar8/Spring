3e58f2dd8f6586d16e0a8cc0352df5ae856d77e9
Nischaya Poddar <nischayapoddar8@gmail.com>
2026-09-01 15:50:34 +0530

    Started working on mini project to understand JPA relationships and transactional annotation

---
+++ 6.JpaRelationshipsTransactionalProject/src/test/java/nischaya/example6/JpaRelationshipsTransactionalProject/ApplicationTests.java
@@ -0,0 +1,13 @@
+package nischaya.example6.JpaRelationshipsTransactionalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
